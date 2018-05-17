6616b7cfd336da45a279e7c32bd453bf89e21e8b
kbsonlong <kbsonlong@gmail.com>
2018-05-17 19:54:43 +0800

    记录失败url

--- links/links/spiders/mfw.py
+++ links/links/spiders/mfw.py
@@ -36,12 +36,26 @@ class DmozSpider(scrapy.Spider):
                 nums = citylist.find('div', {'class': 'nums'}).b.get_text()
                 detail = citylist.find('div', {'class': 'detail'}).get_text().replace(' ', '').strip('\n')
                 image = citylist.find('div', {'class': 'img'}).a.img.get('data-original')
-                top1 = citylist.find('dd').select('a')[0].get_text()
-                top1_url = self.site_url + citylist.find('dd').select('a')[0].get('href')
-                top2 = citylist.find('dd').select('a')[1].get_text()
-                top2_url = self.site_url + citylist.find('dd').select('a')[1].get('href')
-                top3 = citylist.find('dd').select('a')[2].get_text()
-                top3_url = self.site_url + citylist.find('dd').select('a')[2].get('href')
+                top = citylist.find('dd').select('a')
+                if len(top) == 3:
+                    top1 = citylist.find('dd').select('a')[0].get_text()
+                    top1_url = self.site_url + citylist.find('dd').select('a')[0].get('href')
+                    top2 = citylist.find('dd').select('a')[1].get_text()
+                    top2_url = self.site_url + citylist.find('dd').select('a')[1].get('href')
+                    top3 = citylist.find('dd').select('a')[2].get_text()
+                    top3_url = self.site_url + citylist.find('dd').select('a')[2].get('href')
+                elif len(top) == 2:
+                    top1 = citylist.find('dd').select('a')[0].get_text()
+                    top1_url = self.site_url + citylist.find('dd').select('a')[0].get('href')
+                    top2 = citylist.find('dd').select('a')[1].get_text()
+                    top2_url = self.site_url + citylist.find('dd').select('a')[1].get('href')
+                    top3 = top3_url = None
+                elif len(top) == 1:
+                    top1 = citylist.find('dd').select('a')[0].get_text()
+                    top1_url = self.site_url + citylist.find('dd').select('a')[0].get('href')
+                    top2 = top2_url = top3 = top3_url =None
+                else:
+                    top1 = top1_url = top2 = top2_url = top3 = top3_url = None
 
                 item.update({'cityid': cityid, 'city_url': city_url, 'city_name': city_name, 'nums': nums,
                              'detail': detail, 'image': image, 'top1': top1, 'top1_url': top1_url,
